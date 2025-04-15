e8295e039b2a541afcb55edabb47a31734e259a6
John Puksta <john.puksta@gmail.com>
2025-04-15 00:01:42 -0400

    Updated logger registration

--- Source/Gobb/Program.cs
+++ Source/Gobb/Program.cs
@@ -7,20 +7,24 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Logging.ClearProviders();
-builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-builder.Logging.AddConsole();
-builder.Services.AddLogging();
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+builder.Services.AddLogging(b =>
+    b
+        .AddDebug()
+        .AddConsole()
+        .AddConfiguration(builder.Configuration.GetSection("Logging"))
+        .SetMinimumLevel(LogLevel.Information)
+);
+
 builder.Services.Configure<JiraClientOptions>(
     builder.Configuration.GetSection("JiraClientOptions"));
 
-builder.Services.AddTransient<JiraClient>();
-builder.Services.AddTransient<ITicketProvider, JiraTicketProvider>();
+builder.Services.AddSingleton<JiraClient>();
+builder.Services.AddSingleton<ITicketProvider, JiraTicketProvider>();
 
 builder.Services
     .AddMcpServer()
